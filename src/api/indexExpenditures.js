d58b3d536c9229137335bb1bc8833c7f063da79a
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-09-14 12:10:34 +0100

    Add index api that fetches the expenditure user_id

---
+++ src/api/indexExpenditures.js
@@ -0,0 +1,19 @@
+const indexExpenditures = async userId => {
+  const url = `http://localhost:4000/expenditures?user_id=${userId}`;
+  const config = {
+    mode: 'cors',
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  const res = await fetch(url, config);
+  const result = await res.json();
+  return ({
+    error: false,
+    payload: result,
+  });
+};
+
+export default indexExpenditures;
