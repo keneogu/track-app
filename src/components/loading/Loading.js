4c3f4b7eda6a543a543299b81639ed2bfe0a7f71
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-09-14 13:07:28 +0100

    Add loading files

---
+++ src/components/loading/Loading.js
@@ -0,0 +1,12 @@
+import Navbar from '../navbar/Navbar';
+
+const Loading = () => (
+  <div>
+    <Navbar />
+    <div className="loading-container">
+      <h1>Please wait...</h1>
+    </div>
+  </div>
+);
+
+export default Loading;
